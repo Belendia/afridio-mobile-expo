64272005426d6905268b64dd223fccc9fcba5e82
Belendia Serda <belendia@gmail.com>
2021-06-01 07:24:25 +0300

    Add service classes

---
+++ src/services/network/AfridioEndpoints.tsx
@@ -0,0 +1,13 @@
+class AfridioEndpoints {
+  V1 = "v1";
+
+  login = () => `${this.V1}/user/login/`;
+  logout = () => `${this.V1}/user/logout/`;
+  register = () => `${this.V1}/user/register/`;
+  verify = () => `${this.V1}/phone/verify/`;
+  genre = () => `${this.V1}/genres/`;
+  media = (slug: string) => `${this.V1}/medias/${slug}`;
+  home = () => `${this.V1}/home/`;
+}
+
+export default new AfridioEndpoints();
