3339f5ebd2a1ce614ca19dbe7a697e48ff4a7ed2
Belendia Serda <belendia@gmail.com>
2021-06-12 11:51:21 +0300

    Implemented signup screen

--- src/screens/Auth/SignUpScreen.tsx
+++ src/screens/Auth/SignUpScreen.tsx
@@ -1,17 +1,111 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet } from "react-native";
 import { Input, Button } from "react-native-elements";
 import { FontAwesome } from "@expo/vector-icons";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
 
-import { AuthContainer, DateInput, OptionsInput } from "../../components";
+import {
+  AuthContainer,
+  DateInput,
+  OptionsInput,
+  Option,
+  FormError,
+} from "../../components";
 import { SexOptions } from "../../constants/Options";
 import { colors } from "../../constants/Colors";
+import { RootStoreType } from "../../redux/rootReducer";
+import {
+  startRegistration,
+  resetError,
+} from "../../redux/slices/registrationSlice";
+
+let SignUpSchema = Yup.object().shape({
+  name: Yup.string().min(3, "To short!").required("Required"),
+  phone_number: Yup.number()
+    .positive("Phone must be a positive number")
+    .typeError("Phone must be a number")
+    .required("Required")
+    .test(
+      "len",
+      "To short!",
+      (value) =>
+        value != undefined && value != null && value.toString().length >= 6
+    ),
+  date_of_birth: Yup.string().required("Required"),
+  sex: Yup.string().required("Required"),
+  password: Yup.string().min(6, "To short!").required("Required"),
+  password2: Yup.string()
+    .equals([Yup.ref("password")], "Passwords don't match")
+    .required("Required"),
+});
 
 const SignUpScreen = () => {
-  const [name, setName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [password, setPassword] = useState("");
-  const [secondPassword, setSecondPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+  const [sex, setSex] = useState<Option | undefined>(undefined);
+
+  const {
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    values,
+    errors,
+    touched,
+    setFieldValue,
+    setErrors,
+  } = useFormik({
+    initialValues: {
+      name: "",
+      phone_number: "",
+      sex: "",
+      date_of_birth: "",
+      password: "",
+      password2: "",
+    },
+    validationSchema: SignUpSchema,
+    onSubmit: (values) => {
+      dispatch(
+        startRegistration({
+          phone_number: "+" + values.phone_number,
+          name: values.name,
+          date_of_birth: values.date_of_birth,
+          sex: values.sex,
+          password: values.password,
+          password2: values.password2,
+        })
+      );
+    },
+  });
+
+  //redux
+  const { registering, registered, error } = useSelector(
+    (state: RootStoreType) => ({
+      registering: state.registrationReducer.registering,
+      registered: state.registrationReducer.registered,
+      error: state.registrationReducer.error,
+    })
+  );
+
+  useEffect(() => {
+    if (error) {
+      setErrors(error);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    return () => {
+      dispatch(resetError());
+    };
+  }, []);
+
+  useEffect(() => {
+    if (registered) {
+      navigation.navigate("VerifyScreen");
+    }
+  }, [registered]);
 
   return (
     <AuthContainer showLogo={true} title={"Sign Up"}>
@@ -19,7 +113,9 @@ const SignUpScreen = () => {
         placeholder="Name"
         leftIconContainerStyle={{ marginRight: 6 }}
         leftIcon={<FontAwesome name="user" size={20} color={colors.red300} />}
-        onChangeText={(text) => setName(text)}
+        onChangeText={handleChange("name")}
+        onBlur={handleBlur("name")}
+        errorMessage={errors.name}
         style={styles.input}
         inputContainerStyle={styles.inputContainer}
         placeholderTextColor={colors.black700}
@@ -29,7 +125,9 @@ const SignUpScreen = () => {
         placeholder="Phone number"
         leftIconContainerStyle={{ marginRight: 6 }}
         leftIcon={<FontAwesome name="phone" size={20} color={colors.red300} />}
-        onChangeText={(text) => setPhoneNumber(text)}
+        onChangeText={handleChange("phone_number")}
+        onBlur={handleBlur("phone_number")}
+        errorMessage={errors.phone_number}
         style={styles.input}
         inputContainerStyle={styles.inputContainer}
         placeholderTextColor={colors.black700}
@@ -40,20 +138,38 @@ const SignUpScreen = () => {
         title="Birth date"
         bottomDivider={false}
         iconName={"event"}
-        onSubmit={(date) => console.log(date)}
+        errorMessage={errors.date_of_birth}
+        onSubmit={(date) => {
+          console.log(date);
+          setFieldValue(
+            "date_of_birth",
+            date.getFullYear() +
+              "-" +
+              (date.getMonth() + 1) +
+              "-" +
+              date.getDate()
+          );
+        }}
       />
       <OptionsInput
         title={"Sex"}
         iconName={"user-female"}
         values={SexOptions}
+        defaultValue={sex}
         bottomDivider={false}
-        onPress={(selectedSex) => console.log(selectedSex)}
+        errorMessage={errors.sex}
+        onPress={(selectedSex) => {
+          setFieldValue("sex", selectedSex.key);
+          setSex(selectedSex);
+        }}
       />
       <Input
         placeholder="Password"
         leftIconContainerStyle={{ marginRight: 6 }}
         leftIcon={<FontAwesome name="lock" size={20} color={colors.red300} />}
-        onChangeText={(text) => setPassword(text)}
+        onChangeText={handleChange("password")}
+        onBlur={handleBlur("password")}
+        errorMessage={errors.password}
         style={styles.input}
         inputContainerStyle={styles.inputContainer}
         placeholderTextColor={colors.black700}
@@ -64,7 +180,9 @@ const SignUpScreen = () => {
         placeholder="Confirm Password"
         leftIconContainerStyle={{ marginRight: 6 }}
         leftIcon={<FontAwesome name="lock" size={20} color={colors.red300} />}
-        onChangeText={(text) => setSecondPassword(text)}
+        onChangeText={handleChange("password2")}
+        onBlur={handleBlur("password2")}
+        errorMessage={errors.password2}
         style={styles.input}
         inputContainerStyle={styles.inputContainer}
         placeholderTextColor={colors.black700}
@@ -78,9 +196,16 @@ const SignUpScreen = () => {
           paddingVertical: 12,
         }}
         titleStyle={{ fontSize: 16, fontWeight: "600" }}
-        containerStyle={{ marginTop: 10, marginBottom: 20 }}
-        onPress={() => console.log("Handle Sign Up")}
+        containerStyle={{ marginTop: 10, marginBottom: 10 }}
+        onPress={() => {
+          if (!registering) handleSubmit();
+        }}
+        loading={registering}
       />
+
+      {error && typeof error == "string" && (
+        <FormError error={error} style={{ marginBottom: 10 }} />
+      )}
     </AuthContainer>
   );
 };
