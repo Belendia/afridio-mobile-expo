2ddc1e4ac024d3d17303a3a964be63aaa4d97ce3
Belendia Serda <belendia@gmail.com>
2021-06-17 21:22:43 +0300

    Remove unused library

--- src/components/Cards/FeaturedMediaCard.tsx
+++ src/components/Cards/FeaturedMediaCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Image, TouchableOpacity } from "react-native";
+import { StyleSheet, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { LinearGradient } from "expo-linear-gradient";
 import { Ionicons } from "@expo/vector-icons";
